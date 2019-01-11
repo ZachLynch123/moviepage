29ec367ba1f0e3be991a83595412c3901e1f06f6
HankBedford <Zachary.Lynch1337@gmail.com>
2019-01-11 10:38:30 -0800

    Added one section of footer

---
+++ client/movieapp/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+    <footer className="main-footer">
+        <section className="main-footer-top">
+            <header className="main-footer-top-header">
+                <h2>Z M D B</h2>
+            </header>
+            <div className="main-footer-top-item">
+                <h3>portfolio:</h3>
+                <p><a href="https://zachlynch123.github.io/">https://zachlynch123.github.io/</a></p>
+            </div>
+            <div className="main-footer-top-item">
+                <h3>Email:</h3>
+                <p>zachary.lynch1337@gmail.com</p>
+            </div>
+            <div className="main-footer-top-rights">
+                <p>Coded and designed by <a href="https://zachlynch123.github.io/">Zach Lynch</a></p>
+            </div>
+        </section>
+        <section className="main-footer-bottom">
+        
+        </section>
+    </footer>
+    );
+}
+
+export default Footer;
