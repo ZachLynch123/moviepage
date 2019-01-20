2135c47ba702427e685f81075e16f52875d8adcb
HankBedford <Zachary.Lynch1337@gmail.com>
2019-01-20 11:12:31 -0800

    Setting up state for JSON deconstruction

--- movieapp/src/components/Details.js
+++ movieapp/src/components/Details.js
@@ -5,6 +5,12 @@ class Details extends React.Component {
         super(props);
         this.state = {
             id: this.props.location.pathname.slice(-6),
+            backdrop: '',
+            poster: '',
+            title: '',
+            genres: [],
+            overview: '',
+            voteAverage: 0,
             isLoaded: false
         }
 }
@@ -13,6 +19,10 @@ async getDetails() {
     try {
         const data = await fetch(`
         https://api.themoviedb.org/3/movie/${this.state.id}?api_key=f8be595d434ed3dc41d8c73f0760f653&language=en-US`);
+        const jsonData = await data.json();
+        console.log(jsonData);
+        
+
     } catch(e) {
         console.log(e);
         
@@ -20,11 +30,10 @@ async getDetails() {
 }
 
 componentDidMount() {
-
+    this.getDetails();
 }
 
     render() {
-        console.log(this.state)
         return(
             <div>
             <h1 className="red">hello world!</h1>
