612156ca88663ab3bfda031db957cb94659ca16b
HankBedford <Zachary.Lynch1337@gmail.com>
2019-02-15 21:22:56 -0800

    commit

--- movieapp/src/sliderSettings.js
+++ movieapp/src/sliderSettings.js
@@ -38,6 +38,17 @@ module.exports = {
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      }
+    ]
   }
 
 }
