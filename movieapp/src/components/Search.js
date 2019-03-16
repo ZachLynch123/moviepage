c38739167d4bbb37f6f9f04708869fb3193c40d4
HankBedford <Zachary.Lynch1337@gmail.com>
2019-03-15 17:07:19 -0700

    calling to the api

--- movieapp/src/components/Search.js
+++ movieapp/src/components/Search.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import keys from '../keys';
 
 class Search extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            movie: escape(window.location.pathname.split('/').slice(-1)[0])
+            movie: unescape(window.location.pathname.split('/').slice(-1)[0]),
         }
+    }
+
+
+    componentDidMount() {
+        this.getMovieList();
+    }
+
+    async getMovieList() {
+         try {
+             const data = await fetch(`
+             https://api.themoviedb.org/3/search/movie?api_key=${keys.apiKey}&language=en-US&query=${this.state.movie}&page=1&include_adult=false`);
+             const jsonData = await data.json();
+             console.log(this.state.movie);
+             
+             console.log(jsonData.results);
+             
+
+         } catch (e) {
+             console.log(e);
+         }
     }
     
 
     render() {
-        console.log(this.props.location);
         return(
             <div>Hello from Search</div>
         )
